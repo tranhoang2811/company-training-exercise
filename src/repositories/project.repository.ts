54dfb47f622926ce029ff942b45043c12fca1d81
Trần Hoàng <trandanghoang2811@gmail.com>
2022-12-06 19:00:56 +0700

    fix repository and model

--- src/repositories/project.repository.ts
+++ src/repositories/project.repository.ts
@@ -1,9 +1,9 @@
 import {inject, Getter} from '@loopback/core';
-import {DefaultCrudRepository, repository, HasManyRepositoryFactory} from '@loopback/repository';
+import {DefaultCrudRepository, repository, HasManyRepositoryFactory, BelongsToAccessor} from '@loopback/repository';
 import {MongoDataSource} from '../datasources';
-import {Project, ProjectRelations, Task, ProjectUser} from '../models';
+import {Project, ProjectRelations, Task, User} from '../models';
 import {TaskRepository} from './task.repository';
-import {ProjectUserRepository} from './project-user.repository';
+import { UserRepository } from './user.repository';
 
 export class ProjectRepository extends DefaultCrudRepository<
   Project,
@@ -13,15 +13,19 @@ export class ProjectRepository extends DefaultCrudRepository<
 
   public readonly tasks: HasManyRepositoryFactory<Task, typeof Project.prototype.id>;
 
-  public readonly projectUsers: HasManyRepositoryFactory<ProjectUser, typeof Project.prototype.id>;
+  public readonly creator: BelongsToAccessor<User, typeof Project.prototype.id>;
 
+  public readonly updater: BelongsToAccessor<User, typeof Project.prototype.id>;
+  
   constructor(
-    @inject('datasources.mongo') dataSource: MongoDataSource, @repository.getter('TaskRepository') protected taskRepositoryGetter: Getter<TaskRepository>, @repository.getter('ProjectUserRepository') protected projectUserRepositoryGetter: Getter<ProjectUserRepository>,
+    @inject('datasources.mongo') dataSource: MongoDataSource, @repository.getter('TaskRepository') protected taskRepositoryGetter: Getter<TaskRepository>, @repository.getter('UserRepository') protected userRepositoryGetter: Getter<UserRepository>,
   ) {
     super(Project, dataSource);
-    this.projectUsers = this.createHasManyRepositoryFactoryFor('projectUsers', projectUserRepositoryGetter,);
-    this.registerInclusionResolver('projectUsers', this.projectUsers.inclusionResolver);
     this.tasks = this.createHasManyRepositoryFactoryFor('tasks', taskRepositoryGetter,);
     this.registerInclusionResolver('tasks', this.tasks.inclusionResolver);
+    this.creator = this.createBelongsToAccessorFor('creator', userRepositoryGetter)
+    this.registerInclusionResolver('creator', this.creator.inclusionResolver)
+    this.updater = this.createBelongsToAccessorFor('updater', userRepositoryGetter)
+    this.registerInclusionResolver('updater', this.updater.inclusionResolver)
   }
 }
--- src/repositories/task.repository.ts
+++ src/repositories/task.repository.ts
@@ -3,7 +3,6 @@ import {DefaultCrudRepository, repository, BelongsToAccessor} from '@loopback/re
 import {MongoDataSource} from '../datasources';
 import {Task, TaskRelations, Project, User} from '../models';
 import {ProjectRepository} from './project.repository';
-import {TaskRepository} from './task.repository';
 import {UserRepository} from './user.repository';
 
 export class TaskRepository extends DefaultCrudRepository<
@@ -13,20 +12,28 @@ export class TaskRepository extends DefaultCrudRepository<
 > {
 
   public readonly project: BelongsToAccessor<Project, typeof Task.prototype.id>;
+  
+  public readonly assignee: BelongsToAccessor<User, typeof User.prototype.id>
 
-  public readonly task: BelongsToAccessor<Task, typeof Task.prototype.id>;
+  public readonly link: BelongsToAccessor<Task, typeof Task.prototype.id>;
 
-  public readonly user: BelongsToAccessor<User, typeof Task.prototype.id>;
+  public readonly creator: BelongsToAccessor<User, typeof Task.prototype.id>;
+
+  public readonly updater: BelongsToAccessor<User, typeof Task.prototype.id>;
 
   constructor(
     @inject('datasources.mongo') dataSource: MongoDataSource, @repository.getter('ProjectRepository') protected projectRepositoryGetter: Getter<ProjectRepository>, @repository.getter('TaskRepository') protected taskRepositoryGetter: Getter<TaskRepository>, @repository.getter('UserRepository') protected userRepositoryGetter: Getter<UserRepository>,
   ) {
     super(Task, dataSource);
-    this.user = this.createBelongsToAccessorFor('user', userRepositoryGetter,);
-    this.registerInclusionResolver('user', this.user.inclusionResolver);
-    this.task = this.createBelongsToAccessorFor('task', taskRepositoryGetter,);
-    this.registerInclusionResolver('task', this.task.inclusionResolver);
     this.project = this.createBelongsToAccessorFor('project', projectRepositoryGetter,);
     this.registerInclusionResolver('project', this.project.inclusionResolver);
+    this.assignee = this.createBelongsToAccessorFor('assignee', userRepositoryGetter)
+    this.registerInclusionResolver('assignee', this.assignee.inclusionResolver)
+    this.link = this.createBelongsToAccessorFor('link', taskRepositoryGetter)
+    this.registerInclusionResolver('link', this.link.inclusionResolver)
+    this.creator = this.createBelongsToAccessorFor('creator', userRepositoryGetter)
+    this.registerInclusionResolver('creator', this.creator.inclusionResolver)
+    this.updater = this.createBelongsToAccessorFor('updater', userRepositoryGetter)
+    this.registerInclusionResolver('updater', this.updater.inclusionResolver)
   }
 }
